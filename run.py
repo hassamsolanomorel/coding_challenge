e36d0fb62112bf99fe1c3ddfe912d14c3f1f5bdf
Hassam Solano-Morel <37357072+hassamsolanomorel@users.noreply.github.com>
2021-06-14 09:43:32 -0400

    General clean up, minor missing key and code coverage bug fixes (#4)
    * BUGFIX: Fix issue where test container reported no code coverage
    
    * Update some comments to provide clarity on access token in code.
    
    * Add variable for Flask host addr, general clean up, fix missing keys

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from app.application import app
 
 
 if __name__ == '__main__':
-    # Using host 0.0.0.0 makes dockerizing the app just a bit easier
-    app.run(debug=True, host='0.0.0.0')
+    flask_addr = os.environ.get("FLASK_ADDRESS")
+    if flask_addr:
+        # Using host 0.0.0.0 makes dockerizing the app just a bit easier
+        app.run(debug=True, host=flask_addr)
+    else:
+        app.run(debug=True)
